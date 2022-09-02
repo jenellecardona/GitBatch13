7fb6c29aaa828515097963445b9ef17f6155ad1a
jenellecardona <jnel.cardona@gmail.com>
2022-09-01 22:41:35 -0400

    temperature check basic code

---
+++ src/test/java/class01/GitBasics.java
@@ -0,0 +1,11 @@
+package class01;
+
+public class GitBasics {
+    public static void main(String[] args) {
+        /*
+        write down a code that checks the temp from a website
+        And prints out if it's a beautiful day or not.
+         */
+        System.out.println("today is a beautiful day");
+    }
+}
